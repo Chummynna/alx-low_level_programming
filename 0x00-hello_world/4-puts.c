0bad4f6bf84b059983beee001f6dcd71ebaa4386
Chioma Ononyaba <chummynna@gmail.com>
2022-04-07 17:16:00 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
- * main - Entry point
- * 
- * Return: Always 0 (Success)
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *   * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle";
-	
- 	puts(str):	
-	
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
