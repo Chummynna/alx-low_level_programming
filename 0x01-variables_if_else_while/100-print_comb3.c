3d926f539fa1cc52cdd8669a3b816ac0f183199e
Chioma Ononyaba <chummynna@gmail.com>
2022-04-08 17:54:49 -0700

    Inventing is a combination of brains and material

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Prints numbers between 00 to 89.
  *
@@ -6,29 +7,31 @@
  */
 int main(void)
 {
-int i, e;
-i = 48;
-e = 48;
-while (e < 58)
-{
-i = 48;
-while (i < 58)
-}
-if (e != i && e < i)
-{
-putchar(e);
-putchar(i);
-if (i == 57 && e == 56)
-{
-break;
-}
-putchar(',');
-putchar(' ');
-}
-i++;
-}
-e++;
-{
-putchar('\n');
-return (0);
+	int i, e;
+
+	i = 48;
+	e = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{
+																							putchar(e);
+																							putchar(i);
+																							if (i == 57 && e == 56)
+																							{
+																								break;
+																							}
+	         		   putchar(',');
+                                   putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
 }
