6211f5cf72c8234c14a57a529538d052106a6da1
Chioma Ononyaba <chummynna@gmail.com>
2022-04-20 15:47:50 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,7 @@ void rev_string(char *s)
 	int len, i, half;
 	char temp;
 
-	for (len = 0; s[len] != '\n'; len++);
+	for (len = 0; s[len] != '\0'; len++);
 
 	i = 0;
 
@@ -20,7 +20,7 @@ void rev_string(char *s)
 	while (half--)
 	{
 		temp = s[len - i - 1];
-		s[len - 1] = s[i];
+		s[len - i - 1] = s[i];
 		s[i] = temp;
 		i++;
 	}
