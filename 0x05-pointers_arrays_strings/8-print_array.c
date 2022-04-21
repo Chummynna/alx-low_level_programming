3c045f8ff98c48cbceeb31acbe53d438b33b972d
Chioma Ononyaba <chummynna@gmail.com>
2022-04-20 17:22:41 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: pointter to an int type array
+ * @b: return value n
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+int 1;
+
+i = 0;
+while (i < n)
+{
+printf("%d", a[i]);
+
+if (i < n - 1)
+{
+printf(", ");
+}
+
+i++;
+}
+
+printf("\n")
+;
+}
