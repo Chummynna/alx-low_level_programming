e52059552632a4ecd1b6ea6dc5bddff74687d359
Chioma Ononyaba <chummynna@gmail.com>
2022-04-20 17:56:57 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _string - copies the string pointed to by src
+ * @dest: char type string
+ * @src: char type string
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+int i;
+
+i = o;
+
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+
+}
