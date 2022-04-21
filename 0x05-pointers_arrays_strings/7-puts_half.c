14fcac374bcaaff992552f6baddf7fc76fde01d6
Chioma Ononyaba <chummynna@gmail.com>
2022-04-20 17:01:16 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - prints a string, followed by a new line
+ * @str: pointer to the string to print
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+int b, last;
+
+b = ;
+while (str[b] != '\0')
+{
+b++;
+}
+
+last = (b + 1) / 2;
+
+for (b = last; str[b]; b++)
+{
+_putchar (str[b]);
+}
+
+_putchar ('\n');
+
+}
