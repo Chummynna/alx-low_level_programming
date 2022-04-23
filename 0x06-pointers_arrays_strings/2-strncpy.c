c03fc847e89c26a0d8d56bf2f813b4887ee2d1ce
Chioma Ononyaba <chummynna@gmail.com>
2022-04-23 13:10:50 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *_strncpy - copies a string
+ *@dest: pointer to a string
+ *@src: pointer to a string 
+ *@n: num of bytes
+ *Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+int j;
+
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[j] = src[j];
+j++;
+}
+
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
+
+return (dest);
+
+}
