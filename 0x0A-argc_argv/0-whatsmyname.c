a0da11ba1ba04a8f5816eac9c319af5bf56c9e20
Chioma Ononyaba <chummynna@gmail.com>
2022-05-02 07:19:14 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the new name of the program
+ * @argc:insert argument
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+
+return (0);
+}
