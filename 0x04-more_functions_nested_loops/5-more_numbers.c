6f1e24f8c1ff75b6d4cb611b79e1ecb9a229f228
Chioma Ononyaba <chummynna@gmail.com>
2022-05-03 05:41:49 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,19 +2,21 @@
 
 /**
  * more_numbers - prints 10 times the numbers from 0 to 14
+ * followed by a new line
  *
  */
 void more_numbers(void)
-{
-	int n, j;
 
-	for (n = 0; n < 10; n++)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j< 15; j++)
+		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-				_putchar((j / 10) + '0');
 
+			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
