5909f8fa8033bdeac9321260793d3879b60732b8
Chioma Ononyaba <chummynna@gmail.com>
2022-05-05 13:35:54 -0700

    static libraries

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - function finds the first occurrence of the substring
+ * @haystack: The terminating null bytes
+ * @needle: look for the haystack
+ * Return: return 0
+ **/
+
+char  *_strstr(char *haystack, char *needle)
+{
+	char *str1, *str2; /*Declaring variables*/
+
+	while (*haystack != '\0')
+	{
+		str1 = haystack; /*values*/
+		str2 = needle;
+
+		/*Star WHILE*/
+		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
+		{
+
+			haystack++;
+			str2++;
+		}
+		if (*str2 == '\0')
+			return (str1);
+		haystack = str1 + 1;
+	}
+	return (0);
+}
