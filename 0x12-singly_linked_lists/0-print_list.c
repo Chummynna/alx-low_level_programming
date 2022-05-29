c132f895f2416bc5bca7a080a943adb0d0eca72a
Chioma Ononyaba <chummynna@gmail.com>
2022-05-29 06:59:50 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * print_list - prints all the elements  a list_t list.
  * @h: singly linked list.
@@ -6,16 +7,16 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t nelem;
-nelem = 0;
-while (h != NULL)
-{
-if (h->str == NULL)
-printf("[%d] %s\n", 0, "(nil)");
-else
-printf("[%d] %s\n", h->len, h->str);
-h = h->next;
-nelem++;
-}
+	size_t nelem;
+	nelem = 0;
+	while (h != NULL)
+	{
+	if (h->str == NULL)
+		printf("[%d] %s\n", 0, "(nil)");
+	else
+		printf("[%d] %s\n", h->len, h->str);
+	h = h->next;
+	nelem++;
+	}
 return (nelem);
 }
